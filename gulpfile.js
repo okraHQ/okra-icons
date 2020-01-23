514349935e1b931c28fce8636eb2b4bdb96d0507
Benjamin Mark <benjamin@okra.ng>
2020-01-23 12:54:19 +0100

    config file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const env = require('yargs').argv.env;
 var gulp =require('gulp');
 var babel =require('gulp-babel');
+const eslint = require('gulp-eslint');
 
 gulp.task('build', async () => {
-   gulp.src(['src/./*.js','src/**/*.js'])
+   gulp.src(['src/./*.js','src/icons/*.js'])
       .pipe(babel())
       .pipe(gulp.dest(['dist']))
 });
@@ -12,4 +13,14 @@ gulp.task('watch', async () => {
     if(env === 'dev') gulp.watch('./*.js', gulp.series('build'));
 });
 
-gulp.task('start', gulp.series('build', 'watch'));
+gulp.task('lint', function () {
+    return gulp.src(['src/**'])
+      .pipe(eslint({
+        useEslintrc: true,
+        envs: ['node']
+        }))
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
+  });
+
+gulp.task('start', gulp.series('lint','build', 'watch'));
